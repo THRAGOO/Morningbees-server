70c40af2e5d115a03a2fbafd703db1a1acf3c647
JeonJoonHo <jeonjh9@naver.com>
2020-03-04 23:37:38 +0900

    - 사용하지 않는 코드 제거

--- src/main/kotlin/com/morningbees/controller/api/v1/MissionController.kt
+++ src/main/kotlin/com/morningbees/controller/api/v1/MissionController.kt
@@ -3,7 +3,6 @@ package com.morningbees.controller.api.v1
 import com.morningbees.dto.MissionCreateDto
 import com.morningbees.model.User
 import com.morningbees.service.MissionService
-import com.morningbees.util.Token
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -18,14 +17,12 @@ import javax.servlet.http.HttpServletRequest
 class MissionController {
     @Autowired
     lateinit var missionService: MissionService
-    @Autowired
-    lateinit var token: Token
 
     @ResponseBody
     @PostMapping("/missions", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
-    fun create(@RequestParam("file") file: MultipartFile, @ModelAttribute missionCreateDto: MissionCreateDto, request: HttpServletRequest): ResponseEntity<Any> {
+    fun create(@RequestParam("image") image: MultipartFile, @ModelAttribute missionCreateDto: MissionCreateDto, request: HttpServletRequest): ResponseEntity<Any> {
         val user: User = request.getAttribute("user") as User
-        missionService.create(user, file, missionCreateDto)
+        missionService.create(user, image, missionCreateDto)
 
         return ResponseEntity(HttpStatus.CREATED)
     }
