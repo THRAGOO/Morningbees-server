4d66f4d9cffcf5858328b031019fe16f86a65d23
JeonJoonHo <jeonjh9@naver.com>
2020-03-08 15:22:00 +0900

    - 사용하지 않는 코드 제거

--- src/main/kotlin/com/morningbees/util/LogEvent.kt
+++ src/main/kotlin/com/morningbees/util/LogEvent.kt
@@ -30,8 +30,7 @@ enum class LogEvent(val code: String) {
 
     //BeeController Process
     CreateBeeError("CBE001"),
-    //BeeController Process
-    CreateBeeError("CBE001"),
+
     // S3Service Process
     S3ServiceProcessError("SSE001"),
 
