0c6d126a4e22c70839ff8e4ae9cc1b7901845abd
JeonJoonHo <jeonjh9@naver.com>
2020-09-20 18:44:24 +0900

    테스트 용 이미지 업로드 API 추가

--- src/main/kotlin/com/morningbees/controller/HomeController.kt
+++ src/main/kotlin/com/morningbees/controller/HomeController.kt
@@ -1,14 +1,28 @@
 package com.morningbees.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import com.morningbees.service.S3Service
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 import javax.servlet.http.HttpServletRequest
 
 @RestController
-class HomeController {
+class HomeController(
+        val s3Service: S3Service
+) {
 
     @GetMapping("/hello")
     fun hello(request: HttpServletRequest): String {
         return "Hello"
+    }
+
+    @ResponseBody
+    @PostMapping("/image/upload", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun upload(@RequestParam("image") image: MultipartFile): ResponseEntity<HashMap<String, String>> {
+        val imageUrl: String = s3Service.upload(image)
+
+        return ResponseEntity(hashMapOf("image_url" to imageUrl), HttpStatus.CREATED)
     }
 }
