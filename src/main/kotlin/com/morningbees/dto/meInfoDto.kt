155aedba620af0572385a5a12cc4ee4432b71b2c
JeonJoonHo <jeonjh9@naver.com>
2021-01-09 10:28:09 +0900

    Add user id in Me API

--- src/main/kotlin/com/morningbees/dto/meInfoDto.kt
+++ src/main/kotlin/com/morningbees/dto/meInfoDto.kt
@@ -3,5 +3,6 @@ package com.morningbees.dto
 data class meInfoDto(
         val nickname: String = "",
         val alreadyJoin: Boolean? = null,
-        val beeId: Long = 0
+        val beeId: Long = 0,
+        val userId: Long? = 0
 )
